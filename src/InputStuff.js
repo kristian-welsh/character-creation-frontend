a96f68a5d9baec62ee98e603484fac999cb851ca
Kristian Welsh <kriswelsh@gmail.com>
2022-01-29 19:19:37 +0000

    add input stuff against do-backend server endpoint

--- src/InputStuff.js
+++ src/InputStuff.js
@@ -1,15 +1,38 @@
+import {Component} from 'react'
+export default class InputStuff extends Component {
 
-function InputStuff() {
-    return (
-        <div className="InputStuff">
-            <p>Please enter some text:</p>
-            <form action="/endpoint" method="post" className="form">
-                <label>char name:</label>
-                <input id="charName" type="text" name="lolll"></input>
-                <input type="submit" value="Do A Thing"></input>
-            </form>
-        </div>
-    )
+    foo(e) {
+        e.preventDefault()
+
+        fetch("http://localhost:5000/endpoint", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: "george" })
+        }).then(resp => {
+            console.log("worked");
+            resp.json().then(json => {
+                console.log(json);
+            });
+        }).catch(err => {
+            console.log("didn't work");
+            console.log(err);
+        });
+    }
+
+    render() {
+        return (
+            <div className="InputStuff">
+                <p>Please enter some text:</p>
+                <form onSubmit={this.foo} className="form">
+                    <label>char name:</label>
+                    <input id="charName" type="text" name="thename"></input>
+                    <input type="submit" value="Do A Thing"></input>
+                </form>
+            </div>
+        )
+    }
+
 }
-
-export default InputStuff;
