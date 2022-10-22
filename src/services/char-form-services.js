48e43b1952a7547ca0bea98e60c8e1c319e75078
grbh <george.harland@piratech.io>
2022-10-22 22:11:04 +0100

    22/10 stuff

--- src/services/char-form-services.js
+++ src/services/char-form-services.js
@@ -1,27 +1,34 @@
-const CHARACTER_ENDPOINT = "http://localhost:5000/endpoint"
+const CHARACTER_ENDPOINT = "http://localhost:5000/endpoint";
 
 class CharacterFormService {
+  sendForm(characterDetails) {
+    // create body
 
-    sendForm(charName, charLevel){
-        fetch(CHARACTER_ENDPOINT, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name: charName, level: charLevel })
-        }).then(resp => {
-            console.log("worked");
-            resp.json().then(json => {
-                console.log(json);
-            }).catch(err => {
-                console.log(err);
-            });
-        }).catch(err => {
-            console.log("didn't work");
+    fetch(CHARACTER_ENDPOINT, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(characterDetails),
+    })
+      .then((resp) => {
+        console.log(characterDetails);
+        console.log("worked");
+        resp
+          .json()
+          .then((json) => {
+            console.log(json);
+          })
+          .catch((err) => {
             console.log(err);
-        });
-    }
+          });
+      })
+      .catch((err) => {
+        console.log("didn't work");
+        console.log(err);
+      });
+  }
 }
 
-export default new CharacterFormService()
+export default new CharacterFormService();
