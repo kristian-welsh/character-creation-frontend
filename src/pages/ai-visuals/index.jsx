2a8105c285c79abce2f542d3310f0fcaca66f547
grbh <george.harland@piratech.io>
2022-11-24 23:34:31 +0000

    warrior image

--- src/pages/ai-visuals/index.jsx
+++ src/pages/ai-visuals/index.jsx
@@ -3,8 +3,17 @@ import "./styles.css";
 import Button from "./../../components/button";
 
 const AiVisuals = () => {
-  const generateName = () => {
-    console.log("name");
+  const generateName = async () => {
+    try {
+      const res = await fetch(
+        "https://api.fungenerators.com/name/generate?category=elf&limit=1"
+      );
+      const name = res.contents.names[0];
+      console.log(name);
+    } catch (err) {
+      console.log("Name generate request failed.");
+      console.log(err);
+    }
   };
   return (
     <div className="wrapper">
